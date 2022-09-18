f70636f16a6e9eb8fe416a61b897d75a216b34aa
Chris Milbert <cmilbert>
2022-09-18 18:41:49 -0400

    Working through pelican tutorial with DrgnFireYellow's help

---
+++ pelicanconf.py
@@ -0,0 +1,31 @@
+AUTHOR = "cmilbert"
+SITENAME = "cmilbert's site"
+SITEURL = ""
+SITEIMAGE = "https://sdk.bitmoji.com/render/panel/20048676-100448150788_1-s5-v1.png?transparent=1&palette=1&scale=1"
+
+PATH = "content"
+
+TIMEZONE = "EST"
+
+DEFAULT_LANG = "en"
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = ()
+
+# Social widget
+SOCIAL = ()
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+# RELATIVE_URLS = True
+
+THEME = "themes/pelican-alchemy/alchemy"
+BOOTSTRAP_CSS = "https://bootswatch.com/5/flatly/bootstrap.min.css"
